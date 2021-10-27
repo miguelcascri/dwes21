955a12ccc33df4d9c9b2855e10e6529e41317bec
msmc <74257158+msmc98@users.noreply.github.com>
2021-10-27 18:14:58 +0200

    Add files via upload

--- index.php
+++ index.php
@@ -87,8 +87,8 @@
 
         <button class="accordion">TEMA 5</button>
         <div class="panel">
-        <a href="tema 5/ejercicio1.php">EJERCICIO 1</a>
-        <p>Ejemplo sesiones</p>
+        <a href="tema 5/index.php">EJERCICIO 1</a>
+        <p>Ejercicio Sesiones</p>
         </div>
 
         <script src="app.js"></script>
