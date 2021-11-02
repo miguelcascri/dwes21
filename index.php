823acfec8d09e117d8b997b2efe21a002e4df908
msmc <74257158+msmc98@users.noreply.github.com>
2021-11-02 15:25:14 +0100

    Add files via upload

--- index.php
+++ index.php
@@ -1,41 +1,92 @@
-<!DOCTYPE html>
-<html lang="es">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>index.php</title>
-
-</head>
-<body>
-    <p>LOG IN</p>
-        <form method="post" action="login.php">
-        <p>Email</p>      <input type="email" name="usuario">
-        <p>Contraseña</p>  <input type="password" name="pw"><br><br>
+<html>
+    <head>
+        <title>Prueba de PHP</title>
+        <link rel="stylesheet" href="app.css">
+        
+    </head>
+    <style type="text/css">
+        body {
+            margin: 0 auto;
+        }
+    </style>
+    <body>
+        <h1>MANUEL SALVADOR MUÑOZ COBOS</h1>
+        <button class="accordion">TEMA 1</button>
+        <div class="panel">
+        <a href="tema 1/server.js">EJERCICIO 1</a>
+        <p>Crear servidor node.js a través de node.js</p>
+        </div>
+        <button class="accordion">TEMA 2</button>
+        <div class="panel">
+        
+        <a href="tema 2/ejercicio1.php">EJERCICIO 1</a>
+        <p>Variables y tipos de datos</p>
+        <a href="tema 2/ejercicio2.php">EJERCICIO 2</a>
+        <p>Entrecomillado simple y doble</p>
+        <a href="tema 2/ejercicio3.php">EJERCICIO 3</a>
+        <p>Conversión tipos de datos</p>
+        <a href="tema 2/ejercicio4.php">EJERCICIO 4</a>
+        <p>Cadenas de texto</p>
+        <a href="tema 2/ejercicio5.php">EJERCICIO 5</a>
+        <p>Arrays</p>
+        </div>
+        <button class="accordion">TEMA 3</button>
+        <div class="panel">
+        <a href="tema 3/ejercicio1.php">EJERCICIO 1</a>
+        <form method="post" action="tema 3/ejercicio1.php">
+        <input type="text" name="peso">
+        <input type="text" name="altura">
         <input type="submit">
         </form>
-</body>
+        <p>Calculo del IMC</p>
+        <a href="tema 3/ejercicio2.php">EJERCICIO 2</a>
+        <p>Dados</p>
+        <a href="tema 3/ejercicio3.php">EJERCICIO 3</a>
+        <p>Conezión a BBDD de MySQL y tipos de dato de tiempo</p>
+        <a href="tema 3/ejercicio4.php">EJERCICIO 4</a>
+        <p>Ejercicio de Sort (arrays)</p>
+        <a href="tema 3/ejercicio5.php">EJERCICIO 5</a>
+        <p>Contar número de repeticiones de números aleatorios en un array</p>
+        <a href="tema 3/ejercicio6.php">EJERCICIO 6</a>
+        <p>Diferencia de las diagonales de una matriz 10x10 aleatoria</p>
+        <a href="tema 3/ejercicio7.php">EJERCICIO 7</a>
+        <p>Añadir a un array de datos en la BBDD</p>
+        </div>
+        <button class="accordion">TEMA 4</button>
+        <div class="panel">
+        <a href="tema 4/ejercicio1.php">EJERCICIO 1</a>
+        <form method="post" action="tema 4/ejercicio1.php">
+        <input type="number" name="number">
+        <input type="submit">
+        </form>
+        <p>Ejercicio de primos</p>
+        <a href="tema 4/ejercicio2.php">EJERCICIO 2</a>
+        <form method="post" action="tema 4/ejercicio2.php">
+        <input type="date" name="date">
+        <input type="submit">
+        </form>
+        <p>Ejercicio del año bisiesto</p>
+        <a href="tema 4/ejercicio3.php">EJERCICIO 3</a>
+        <form method="post" action="tema 4/ejercicio3.php">
+        <label>Introduzca el número de elementos de la serie de fibonacci que desea obtener</label>
+        <input type="number" name="fibonacci">
+        <input type="submit">
+        </form>
+        <p>Ejercicio de la serie de fibonacci</p>
+        <a href="tema 4/ejercicio4.php">EJERCICIO 4</a>
+        <form method="post" action="tema 4/ejercicio4.php">
+        <p>Email      <input type="email" name="usuario"></p>
+        <p>Contraseña  <input type="password" name="pw"></p>
+        <input type="submit">
+        </form>
+        <p>Ejercicio de usuario existente</p>
+        </div>
+        <button class="accordion">TEMA 5</button>
+        <div class="panel">
+        <a href="tema 5/index.php">EJERCICIO 1</a>
+        <p>Ejercicio Sesiones</p>
+        </div>
+        <script src="app.js"></script>
+    </body>
+    
 </html>
-
-<?php
-
-if(isset($_GET["msj"])){
-
-    if($_GET["msj"]=="1"){
-        echo '<script language="javascript">alert("Error, autenticación incorrecta.");</script>';
-    }
-
-    if($_GET["msj"]=="2"){
-        echo '<script language="javascript">alert("Sesión cerrada con éxito.");</script>';
-    }
-
-    if($_GET["msj"]=="3"){  
-        echo '<script language="javascript">alert("No se ha iniciado sesión.");</script>';
-    }
-
-    if($_GET["msj"]=="4"){  
-        echo '<script language="javascript">alert("La sesión ha caducado.");</script>';
-    }
-}
-
-?>
