0189dcbbd2d19820d4d2d32bd6a270e5b9c62f00
msmc98 <manu98mc@gmail.com>
2021-11-03 17:15:37 +0100

    last version

--- tema 5/logout.php
+++ tema 5/logout.php
@@ -1,9 +1,10 @@
 <?php
 
+session_start();
 session_destroy();
+setcookie("color","black",time()-6000000000000000000);
+// header("Location: "."index.php");
 header("Location: "."index.php?msj=2");
 // Destrucción de la cookie, para resetear el valor del color del texto
-setcookie("color","black",time()-60);
-// header("Location: "."index.php");
 
 ?>
