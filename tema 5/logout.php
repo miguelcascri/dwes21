95718dfc71aad270a3c6fb97aabc2b644dfedf23
msmc <74257158+msmc98@users.noreply.github.com>
2021-11-04 13:05:55 +0100

    Add files via upload

--- tema 5/logout.php
+++ tema 5/logout.php
@@ -2,9 +2,9 @@
 
 session_start();
 session_destroy();
+// Destrucción de la cookie, para resetear el valor del color del texto
 setcookie("color","black",time()-6000000000000000000);
 // header("Location: "."index.php");
 header("Location: "."index.php?msj=2");
-// Destrucción de la cookie, para resetear el valor del color del texto
 
 ?>
