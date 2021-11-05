ea76c2be60a0b85af77b34c044d1f26e9571b1c6
msmc <74257158+msmc98@users.noreply.github.com>
2021-11-05 10:50:12 +0100

    Add files via upload

--- tema 5/oauth/logout.php
+++ tema 5/oauth/logout.php
@@ -9,6 +9,9 @@
     //Destroy entire session data.
     session_destroy();
     
+    //Acabamos con la cookie de color
+    setcookie("color","black",time()-6000000000000000000);
+
     //redirect page to index.php
     header('location:../index.php');
 ?>
