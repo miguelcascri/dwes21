cb208781aabfc0fc6d4c55e7f2d9989faa1014ff
msmc <74257158+msmc98@users.noreply.github.com>
2021-11-05 20:10:32 +0100

    Add files via upload

--- tema 5/index.php
+++ tema 5/index.php
@@ -1,5 +1,7 @@
 <?php
 
+require 'oauth/config.php';
+
 if(isset($_GET["msj"])){
 
     if($_GET["msj"]=="1"){
@@ -36,9 +38,14 @@ if(isset($_GET["msj"])){
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <meta content='width=device-width, initial-scale=1, maximum-scale=1' name='viewport'/>
+    <meta name="google-signin-client_id" content="446086727246-95hv3o8l9hnimpopmqikfl91i6i7m4u2.apps.googleusercontent.com">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet" />
     <title>index.php</title>
-
 </head>
+
 <body style="margin: 10%">
     <p>LOG IN</p>
         <form method="post" action="login.php">
@@ -46,9 +53,17 @@ if(isset($_GET["msj"])){
         <p>Contraseña</p>  <input type="password" name="pw"><br><br>
         <input type="submit"><br>
         </form>
-        <form action="oauth/login.php"><br>
-        <p>Si quieres iniciar sesión con tu cuenta de Google...:</p>
-        <input type="submit" value="Google">
+        <br>
+        
+        <div class="container">
+        <a href="https://accounts.google.com/o/oauth2/auth/oauthchooseaccount?response_type=code&access_type=online&client_id=530843722092-f4ljk225nm5rhqjc7ad15cdkifkb0ja7.apps.googleusercontent.com&redirect_uri=http%3A%2F%2Flocalhost%2Fdwes21%2Ftema%25205%2Foauth%2Fgoogle-callback.php&state&scope=email%20profile&approval_prompt=auto&flowName=GeneralOAuthFlow">
+            <div class="panel panel-default" style="width: 110px; padding: 5px; text-align: center; margin-left: 12px;" >
+            <img width='15px' style='margin-bottom:3px;' alt='Google sign-in' 
+            src='https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Google_%22G%22_Logo.svg/512px-Google_%22G%22_Logo.svg.png' />
+            Inicia sesión con Google</a>
+            </div>
+
+        </div>
         </form>
 </body>
 </html>
